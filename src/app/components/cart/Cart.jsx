bed09256e29f604ea8e534b36563014ce6163f58
alphasierradelta <abdousamath2849@gmail.com>
2023-02-28 16:50:21 +0000

    push

--- src/app/components/cart/Cart.jsx
+++ src/app/components/cart/Cart.jsx
@@ -1,17 +1,49 @@
+import { useEffect, useState } from "react";
+import api from "../../../api/api";
+import { Slices } from "./Slices";
 import { Table } from "./Table";
 
 export const CartWidget = () => {
+  const [selectedType,setSlectedType]=useState(0);
+  const [showBoxes,setShowBoxes]=useState("");
+  const [showSlices,setShowSlices]=useState("active");
+  const [boxes,setBoxes]=useState([]);
+  const [slices,setSlices]=useState([]);
+  const setSelection=(selected)=>{
+    console.log('clicked slices')
+    setSlectedType(selected);
+    if(selected==1){
+      setShowBoxes("active");
+      setShowSlices("");
+    }
+    else{
+      setShowSlices("active");
+      setShowBoxes("");
+    }
+    console.log(selectedType);
+  }
+  useEffect(() => {
+    api.get("get-cart/").then((res) => {
+      setBoxes(res.data['boxes']);
+      setSlices(res.data['slices']);
+    });
+  }, []);
+  console.log(slices);
   return (
     <div className="card-body pt-0">
-      <div className="table-responsive mb-8">
-        <Table />
+      {selectedType==1 && <div className="table-responsive mb-8">
+        <Table boxes={boxes}/>
       </div>
+      }
+      {selectedType==0 && <div className="table-responsive mb-8">
+        <Slices slices={slices} />
+      </div>}
       <div className="d-flex flex-stack bg-success rounded-3 p-6 mb-11">
         <div className="fs-6 fw-bold text-white">
-          <span className="d-block lh-1 mb-2">Subtotal</span>
-          <span className="d-block mb-2">Discounts</span>
-          <span className="d-block mb-9">Tax(12%)</span>
-          <span className="d-block fs-2qx lh-1">Total</span>
+          <span className="d-block lh-1 mb-2">Total des Boxes</span>
+          <span className="d-block mb-2">Total des découpes</span>
+          <span className="d-block mb-9">Frais(2%)</span>
+          <span className="d-block fs-2qx lh-1">Total net à payer</span>
         </div>
         <div className="fs-6 fw-bold text-white text-end">
           <span className="d-block lh-1 mb-2" data-kt-pos-element="total">
@@ -32,24 +64,22 @@ export const CartWidget = () => {
         <h1 className="fw-bold text-gray-800 mb-5">Types</h1>
         <div
           className="d-flex flex-equal gap-5 gap-xxl-9 px-0 mb-12"
-          data-kt-buttons="true"
-          data-kt-buttons-target="[data-kt-button]"
         >
           <label
-            className="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4"
-            data-kt-button="true"
+            className={"btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4 "+showBoxes}
+            data-kt-button="true" onClick={()=>setSelection(1)}
           >
             <input className="btn-check" type="radio" name="method" value="0" />
             <i className="fonticon-cash-payment fs-2hx mb-2 pe-0"></i>
-            <span className="fs-7 fw-bold d-block">Cash</span>
+            <span className="fs-7 fw-bold d-block">Boxes</span>
           </label>
           <label
-            className="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4 active"
-            data-kt-button="true"
+            className={"btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4 "+showSlices}
+           onClick={()=>setSelection(0)}
           >
             <input className="btn-check" type="radio" name="method" value="1" />
             <i className="fonticon-card fs-2hx mb-2 pe-0"></i>
-            <span className="fs-7 fw-bold d-block">Card</span>
+            <span className="fs-7 fw-bold d-block">Découpes</span>
           </label>
           <label
             className="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4"
@@ -60,7 +90,7 @@ export const CartWidget = () => {
             <span className="fs-7 fw-bold d-block">E-Wallet</span>
           </label>
         </div>
-        <button className="btn btn-primary fs-1 w-100 py-4">Print Bills</button>
+        <button className="btn btn-primary fs-1 w-100 py-4">Commander</button>
       </div>
     </div>
   );
--- src/app/components/cart/Table.jsx
+++ src/app/components/cart/Table.jsx
@@ -2,70 +2,127 @@ import { useEffect, useState } from "react";
 import api from "../../../api/api";
 import { Subtable } from "./Subtable";
 
-export const Table = () => {
-  const [boxes, setBoxes] = useState([]);
+export const Table = ({boxes}) => {
   const [selectedRow, setSelectedRow] = useState(null);
-  const [subtableData, setSubtableData] = useState([]);
-
-  useEffect(() => {
-    api.get("get-cart/").then((res) => {
-      setBoxes(res.data);
-    });
-  }, []);
-  console.log(boxes);
   // function to handle row click and set the selectedRow state
-  const handleRowClick = (rowData) => {
-    console.log('clicked');
-    if (selectedRow === rowData) {
-      setSelectedRow(null);
-    } else {
-      setSelectedRow(rowData);
-      setSubtableData(rowData.box_slices); // assuming each row has subtableData property
-    }
-  };
   return (
-    <table className="table align-middle gs-0 gy-4 my-0">
-      <thead>
-        <tr>
-          <th className="min-w-175px"></th>
-          <th className="w-125px"></th>
-          <th className="w-60px"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {boxes.map((box) => (
-          <tr key={box.id} onClick={()=>handleRowClick(box)} data-kt-pos-element="item">
-            <td className="pe-0">
-              <div className="d-flex align-items-center">
-                <img
-                  src="https://th.bing.com/th/id/OIP.6p6Ng7_VxntWhi2v21sNNAHaE8?pid=ImgDet&rs=1"
-                  className="w-50px h-50px rounded-3 me-3"
-                  alt=""
-                />
-                <span className="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">
-                  {box.type.wording}
+    // <table className="table align-middle gs-0 gy-4 my-0">
+    //   <thead>
+    //     <tr>
+    //       <th className="min-w-175px"></th>
+    //       <th className="w-125px"></th>
+    //       <th className="w-60px"></th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {boxes.map((box) => (
+    //       <tr key={box.id} onClick={()=>handleRowClick(box)} data-kt-pos-element="item">
+    //         <td className="pe-0">
+    //           <div className="d-flex align-items-center">
+    //             <img
+    //               src="https://th.bing.com/th/id/OIP.6p6Ng7_VxntWhi2v21sNNAHaE8?pid=ImgDet&rs=1"
+    //               className="w-50px h-50px rounded-3 me-3"
+    //               alt=""
+    //             />
+    //             <span className="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">
+    //               {box.type.wording}
+    //             </span>
+    //           </div>
+    //         </td>
+    //         <td className="text-end">
+    //           <span
+    //             className="fw-bold text-primary fs-2"
+    //             data-kt-pos-element="item-total"
+    //           >
+    //             {box.cart_id}
+    //           </span>
+    //         </td>
+    //       </tr>
+
+    //     ))
+    //     }
+    //   </tbody>
+    //   {selectedRow && <tr>
+    //     <td colSpan="2">
+    //       <Subtable slices={subtableData}/>
+    //     </td>
+    //   </tr>
+    // }
+    // </table>
+    <div className="accordion accordion-icon-toggle" id="kt_accordion_2">
+      {
+      boxes.map((box)=>(
+        <div key={box.id} className="mb-5">
+        <div
+          className="accordion-header py-3 d-flex"
+          data-bs-toggle="collapse"
+          data-bs-target={"#kt_accordion_2_item_"+box.id}
+        >
+          <span className="accordion-icon">
+            <span className="svg-icon svg-icon-4">
+              <svg>...</svg>
+            </span>
+          </span>
+          <h3 className="fs-4 fw-semibold mb-0 ms-4">
+            {box.type.wording}
+          </h3>
+        </div>
+        <div
+          id={'kt_accordion_2_item_'+box.id}
+          className="fs-6 collapse ps-10"
+          data-bs-parent="#kt_accordion_2"
+        >
+          <tbody>
+            <Subtable slices={box.box_slices}/>
+            {/* {box.box_slices.map((slice)=>
+              <tr key={slice.id} data-kt-pos-element="item">
+              <td className="pe-0">
+                <div className="d-flex align-items-center">
+                  <img
+                    src="https://th.bing.com/th/id/OIP.6p6Ng7_VxntWhi2v21sNNAHaE8?pid=ImgDet&rs=1"
+                    className="w-50px h-50px rounded-3 me-3"
+                    alt=""
+                  />
+                  <span className="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">
+                    {slice.slice.wording}
+                  </span>
+                </div>
+              </td>
+              <td className="pe-0">
+                <div
+                  className="position-relative d-flex align-items-center"
+                  data-kt-dialer="true"
+                  data-kt-dialer-min="1"
+                  data-kt-dialer-max="10"
+                  data-kt-dialer-step="1"
+                  data-kt-dialer-decimals="0"
+                >
+                  <input
+                    type="text"
+                    className="form-control border-0 text-center px-0 fs-3 fw-bold text-gray-800 w-30px"
+                    data-kt-dialer-control="input"
+                    placeholder="Amount"
+                    name="manageBudget"
+                    readonly="readonly"
+                    value={slice.quantity}
+                  />
+                </div>
+              </td>
+              <td className="text-end">
+                <span
+                  className="fw-bold text-primary fs-2"
+                  data-kt-pos-element="item-total"
+                >
+                  {slice.slice.price}
                 </span>
-              </div>
-            </td>
-            <td className="text-end">
-              <span
-                className="fw-bold text-primary fs-2"
-                data-kt-pos-element="item-total"
-              >
-                {box.cart_id}
-              </span>
-            </td>
-          </tr>  
-                 
-        ))
-        }
-      </tbody>
-      {selectedRow && <tr>
-        <td colSpan="2">
-          <Subtable slices={subtableData}/>
-        </td>
-      </tr>
+              </td>
+              </tr>
+            )} */}
+          </tbody>
+        </div>
+      </div>
+      ))
     }
-    </table>
+    </div>
   );
 };
