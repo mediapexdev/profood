60c0b9b97a0de87829e07e82b3278404722e0d00
ziz9219 <aziz922010@gmail.com>
2023-03-12 22:32:32 +0000

    Quelques modifs du 3/12/2023

--- src/api/api.js
+++ src/api/api.js
@@ -3,13 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-<<<<<<< HEAD
       ? "https://api-profood.herokuapp.com/"
       : "http://192.168.1.6:8000/api/",
-=======
-      ? "https://api-profood.herokuapp.com/api/"
-      : "http://localhost:8000/api/",
->>>>>>> 46e0301d92f08045b45ff40d63b3f064107b24a9
   
   // withCredentials: true,
   // headers: {
