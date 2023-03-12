06a20e2c9266de7d52b11b657b2b119cc926a3f2
alphasierradelta <abdousamath2849@gmail.com>
2023-03-12 22:08:07 +0000

    //

--- src/api/api.js
+++ src/api/api.js
@@ -6,10 +6,10 @@ export default axios.create({
       ? "https://api-profood.herokuapp.com/api/"
       : "http://localhost:8000/api/",
   
-  withCredentials: true,
-  headers: {
-    Accept: "*/*",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin" : 'https://profood.vercel.app'
-  },
+  // withCredentials: true,
+  // headers: {
+  //   Accept: "*/*",
+  //   "Content-Type": "application/json",
+  //   "Access-Control-Allow-Origin" : 'http://127.0.0.1:3000'
+  // },
 });
