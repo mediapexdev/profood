13f952c8711c8aad4510be68d47a6b375d73d4de
alphasierradelta <abdousamath2849@gmail.com>
2023-03-13 11:19:26 +0000

    //

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://api-profood.herokuapp.com/"
-      : "http://192.168.1.6:8000/api/",
+      : "http://localhost:8000/api/",
   
   // withCredentials: true,
   // headers: {
