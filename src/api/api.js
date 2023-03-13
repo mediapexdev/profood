096446aabedab659a336b272d5142f1d5e589152
alphasierradelta <abdousamath2849@gmail.com>
2023-03-13 11:20:11 +0000

    //

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api-profood.herokuapp.com/"
+      ? "https://api-profood.herokuapp.com/api/"
       : "http://localhost:8000/api/",
   
   // withCredentials: true,
